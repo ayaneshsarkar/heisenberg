c5b1807ca1e175cd00d7890a43c43ff97715146d
ayanesh <ayaneshofficial@gmail.com>
2020-03-07 01:38:07 +0530

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php 
+
+  // DB Params
+  define('DB_HOST', '__HOSTNAME__');
+  define('DB_USER', '__USERNAME__');
+  define('DB_PASS', '__PASSWORD__');
+  define('DB_NAME', '__DBNAME__');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+
+  // URL Root
+  define('URLROOT', '__URL__');
+
+  // Site Name
+  define('SITENAME', '__SITENAME__');
+  
